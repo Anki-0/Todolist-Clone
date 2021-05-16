974568bdd46560d82f8556a750c255aec24b87d3
Ankit Singh <singhankit25apr@gmail.com>
2021-05-16 21:49:02 +0530

    added Project Overlay and Main Task

---
+++ src/components/ProjectOverlay.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useProjectsValue } from '../context';
+
+export const ProjectOverlay = ({
+  setProject,
+  showProjectOverlay,
+  setShowProjectOverlay,
+}) => {
+  const { projects } = useProjectsValue();
+
+  console.log(projects, showProjectOverlay);
+  return (
+    projects &&
+    showProjectOverlay && (
+      <div className="project-overlay">
+        <ul className="project-overlay__list">
+          {projects.map((project) => (
+            <li
+              key={project.projectid}
+              onClick={() => {
+                setProject(project.projectid);
+                setShowProjectOverlay(false);
+              }}
+            >
+              {project.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  );
+};
